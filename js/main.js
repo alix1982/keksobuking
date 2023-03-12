6f3e13dde5e92a8141a8d599cf59513dddb2e144
Alix2019 <alix1982@yandex.ru>
2023-03-12 15:12:20 +0300

    Update functions

--- js/main.js
+++ js/main.js
@@ -1,35 +1,29 @@
-let integerRnd;
-
-let integerRndRange = function (firstInteger = 1, lastInteger = 100) {
-  if (firstInteger < 0 || lastInteger < 0 || lastInteger<firstInteger) {
+const integerRndRange = (firstInteger = 1, lastInteger = 100) => {
+  if (firstInteger < 0 || lastInteger < 0) {
     alert('Ошибка диапазона, включен диапазон по умолчанию [1; 100]')
     firstInteger = 1;
     lastInteger = 100;
   }
+
+  (lastInteger<firstInteger) && ([firstInteger, lastInteger] = [lastInteger, firstInteger]);
+
   firstInteger = Math.ceil(firstInteger);
   lastInteger = Math.floor(lastInteger);
-  integerRnd = Math.round(Math.random() * (lastInteger - firstInteger) + firstInteger);
-  // console.log(firstInteger);
-  // console.log(lastInteger);
-  // console.log(integerRnd);
-  return integerRnd;
+
+  return Math.round(Math.random() * (lastInteger - firstInteger) + firstInteger);
 }
 integerRndRange(1, 2);
 
-let integerRndFractional;
 
-let integerRndRangeFractional = function (firstInteger = 1, lastInteger = 100, accuracy = 1) {
-  if (firstInteger < 0 || lastInteger < 0 || lastInteger<firstInteger) {
-    alert('Ошибка диапазона, включен диапазон по умолчанию [1; 100], с точностью до одного знака после запятой')
+const integerRndRangeFractional = (firstInteger = 1, lastInteger = 100, accuracy = 1) => {
+  if (firstInteger < 0 || lastInteger < 0) {
+    alert('Ошибка диапазона, включен диапазон по умолчанию [1; 100]')
     firstInteger = 1;
     lastInteger = 100;
   }
-  // firstInteger = Math.ceil(firstInteger);
-  // lastInteger = Math.floor(lastInteger);
-  integerRndFractional = Math.round((Math.random() * (lastInteger - firstInteger) + firstInteger)*(10**accuracy))/10**accuracy;
-  // console.log(firstInteger);
-  // console.log(lastInteger);
-  // console.log(integerRndFractional);
-  return integerRndFractional;
+
+  (lastInteger<firstInteger) && ([firstInteger, lastInteger] = [lastInteger, firstInteger]);
+
+  return Math.round((Math.random() * (lastInteger - firstInteger) + firstInteger)*(10**accuracy))/10**accuracy;
 }
 integerRndRangeFractional(1.12345, 10.2345678, 10);
